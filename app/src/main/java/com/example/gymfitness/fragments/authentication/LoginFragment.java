b60c0ed65379c51fb5405b46257fc2777301bbf0
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2024-08-29 16:32:17 +0700

    update validation

--- app/src/main/java/com/example/gymfitness/fragments/authentication/LoginFragment.java
+++ app/src/main/java/com/example/gymfitness/fragments/authentication/LoginFragment.java
@@ -3,7 +3,6 @@ package com.example.gymfitness.fragments.authentication;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
@@ -12,17 +11,19 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
-import android.util.Patterns;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.example.gymfitness.R;
 import com.example.gymfitness.Users;
+import com.example.gymfitness.activities.HomeActivity;
 import com.example.gymfitness.databinding.FragmentLoginBinding;
+import com.example.gymfitness.helpers.ValidationEmail;
 import com.example.gymfitness.viewmodels.AuthViewModel;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -36,10 +37,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -53,17 +52,15 @@ public class LoginFragment extends Fragment {
     private AuthViewModel viewModel;
     private FragmentLoginBinding binding;
     private NavController navController;
-    ImageButton imgGG;
-    FirebaseAuth auth;
-    FirebaseDatabase database;
-    GoogleSignInClient nGoogleSignInClient;
-    ProgressDialog progressDialog;
-
-    private CallbackManager callbackManager;
+    private ImageButton imgGG;
     private ImageButton imgFB;
+    private FirebaseAuth auth;
+    private FirebaseDatabase database;
+    private GoogleSignInClient nGoogleSignInClient;
+    private ProgressDialog progressDialog;
+    private CallbackManager callbackManager;
 
-
-    int RC_SIGN_IN = 40;
+    private static final int RC_SIGN_IN = 40;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,11 +72,10 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
-        viewModel = new ViewModelProvider(getActivity()).get(AuthViewModel.class);
+        viewModel = new ViewModelProvider(requireActivity()).get(AuthViewModel.class);
         binding.setViewModel(viewModel);
         binding.setLifecycleOwner(this);
-        navController = NavHostFragment.findNavController(LoginFragment.this);
-
+        navController = NavHostFragment.findNavController(this);
         return binding.getRoot();
     }
 
@@ -87,86 +83,30 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View view) {
-                if (validateInput()) {
-                    // Nếu dữ liệu hợp lệ, hiển thị thông báo thành công + xử lí tiếp...
-                    Toast.makeText(getActivity(), "Login successful", Toast.LENGTH_SHORT).show();
-                } else {
-                    // Nếu dữ liệu không hợp lệ, hiển thị thông báo lỗi
-                    Toast.makeText(getActivity(), "Invalid input, login failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-            private boolean validateInput () {
-                String usernameOrEmail = binding.edtUsername.getText().toString().trim();
-                String password = binding.edtPassword.getText().toString().trim();
+        initializeFirebase();
+        initializeGoogleSignIn();
+        initializeFacebookLogin();
+        setupUI();
+    }
 
-                // Kiểm tra Username hoặc Email
-                if (usernameOrEmail.isEmpty()) {
-                    binding.edtUsername.setError("Username or Email cannot be empty");
-                    return false;
-                }
-
-                // Kiểm tra định dạng email nếu input có chứa ký tự '@'
-                if (usernameOrEmail.contains("@") && !Patterns.EMAIL_ADDRESS.matcher(usernameOrEmail).matches()) {
-                    binding.edtUsername.setError("Invalid email format");
-                    return false;
-                }
-
-                // Kiểm tra Password
-                if (password.isEmpty()) {
-                    binding.edtPassword.setError("Password cannot be empty");
-                    return false;
-                }
-
-                if (password.length() < 6) {
-                    binding.edtPassword.setError("Password must be at least 6 characters long");
-                    return false;
-                }
-
-                // Nếu tất cả đều hợp lệ
-                binding.edtUsername.setError(null);
-                binding.edtPassword.setError(null);
-                return true;
-            }
-        });
-
-        imgGG = binding.btnGoogle;
-        imgFB = binding.btnFacebook;
+    private void initializeFirebase() {
         auth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
+    }
 
-        progressDialog = new ProgressDialog(getContext());
-        progressDialog.setTitle("Đăng nhập");
-        progressDialog.setMessage("Đang đăng nhập...");
-
+    private void initializeGoogleSignIn() {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
+        nGoogleSignInClient = GoogleSignIn.getClient(requireContext(), gso);
+    }
 
-        nGoogleSignInClient = GoogleSignIn.getClient(getContext(), gso);
 
-        imgGG.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                login();
-            }
-        });
 
-        // Khởi tạo CallbackManager
+
+    private void initializeFacebookLogin() {
         callbackManager = CallbackManager.Factory.create();
-
-        // Facebook Login
-        imgFB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginManager.getInstance().logInWithReadPermissions(LoginFragment.this, Arrays.asList("email", "public_profile"));
-            }
-        });
-
-
         LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
@@ -183,181 +123,127 @@ public class LoginFragment extends Fragment {
                 Toast.makeText(getContext(), "Đăng nhập Facebook không thành công.", Toast.LENGTH_SHORT).show();
             }
         });
-
-        // Thiết lập sự kiện cho TextView để chuyển đến SignUpFragment
-        TextView tvSignUp = binding.tvSignUp;
-        tvSignUp.setOnClickListener(new View.OnClickListener() {
-
-
-            @Override
-            public void onClick(View v) {
-                navController.navigate(R.id.action_loginFragment_to_signUpFragment);
-            }
-        });
-
     }
 
-    private void login() {
+    private void setupUI() {
+        imgGG = binding.btnGoogle;
+        imgFB = binding.btnFacebook;
+        progressDialog = new ProgressDialog(getContext());
+
+        imgGG.setOnClickListener(v -> loginWithGoogle());
+        imgFB.setOnClickListener(v -> LoginManager.getInstance().logInWithReadPermissions(LoginFragment.this, Arrays.asList("email", "public_profile")));
+
+        TextView tvSignUp = binding.tvSignUp;
+        tvSignUp.setOnClickListener(v -> navController.navigate(R.id.action_loginFragment_to_signUpFragment));
+
+        Button btnLogin = binding.btnLogin;
+        btnLogin.setOnClickListener(v -> {
+            String usernameOrEmail = binding.edtUsername.getText().toString().trim();
+            String password = binding.edtPassword.getText().toString().trim();
+
+
+            boolean isValid = ValidationEmail.validateInputEmail(usernameOrEmail, password, binding.edtUsername, binding.edtPassword);
+
+            if (isValid) {
+                loginWithEmail(usernameOrEmail, password);
+            }
+        });
+    }
+    private void loginWithEmail(String usernameOrEmail, String password) {
+        progressDialog.setMessage("Logging in...");
+        progressDialog.show();
+
+        auth.signInWithEmailAndPassword(usernameOrEmail, password)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    progressDialog.dismiss();
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        saveUserToDatabase(user);
+                        navController.navigate(R.id.action_loginFragment_to_homeFragment);
+                    } else {
+                        Toast.makeText(getContext(), "Login failed: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+
+    private void loginWithGoogle() {
+        //debug loi xem toi dau
+        Log.d("test", "Google Sign-In initiated");
         Intent intent = nGoogleSignInClient.getSignInIntent();
         startActivityForResult(intent, RC_SIGN_IN);
     }
 
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        saveUserToDatabase(user);
+                        navController.navigate(R.id.action_loginFragment_to_homeFragment);
+                    } else {
+                        Toast.makeText(getContext(), "Xác thực không thành công.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(requireActivity(), task -> {
+                    if (task.isSuccessful()) {
+                        FirebaseUser user = auth.getCurrentUser();
+                        saveUserToDatabase(user);
+                        Intent intent = new Intent(getContext(), HomeActivity.class);
+                        startActivity(intent);
+                        progressDialog.dismiss();
+                    } else {
+                        Toast.makeText(getContext(), "Xác thực không thành công.", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                    }
+                })
+                .addOnFailureListener(e -> {
+                    // Handle any exceptions during the authentication process
+                    Toast.makeText(getContext(), "Google Sign-In failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                });
+    }
+
+    private void saveUserToDatabase(FirebaseUser user) {
+        Users users = new Users();
+        users.setUserId(user.getUid());
+        users.setName(user.getDisplayName());
+        users.setProfile(user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "default");
+
+        database.getReference().child("Users").child(user.getUid()).setValue(users);
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // Xử lý kết quả đăng nhập Facebook
-        if (callbackManager != null) {
-            callbackManager.onActivityResult(requestCode, resultCode, data);
-        }
-
-
-        // Xử lý kết quả đăng nhập Google
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account.getIdToken());
             } catch (ApiException e) {
-                e.printStackTrace();
-                Toast.makeText(getContext(), "Đăng nhập không thành công.", Toast.LENGTH_SHORT).show();
+                Log.w("LoginFragment", "Google sign in failed", e);
+
+                Toast.makeText(getContext(), "Google Sign-In failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
             }
+        } else {
+
+            callbackManager.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = auth.getCurrentUser();
-                            Users users = new Users();
-                            users.setUserId(user.getUid());
-                            users.setName(user.getDisplayName());
-                            users.setProfile(user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "default");
-
-                            database.getReference().child("Users").child(user.getUid()).setValue(users);
-
-                            navController.navigate(R.id.action_loginFragment_to_homeFragment);
-
-                        } else {
-                            Toast.makeText(getContext(), "Xác thực không thành công.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-    }
-//
-    private void handleFacebookAccessToken(AccessToken token) {
-        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = auth.getCurrentUser();
-                            Users users = new Users();
-                            users.setUserId(user.getUid());
-                            users.setName(user.getDisplayName());
-                            users.setProfile(user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : "default");
-
-                            database.getReference().child("Users").child(user.getUid()).setValue(users);
-                            navController.navigate(R.id.action_loginFragment_to_homeFragment);
-
-                        } else {
-                            Toast.makeText(getContext(), "Xác thực không thành công.", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
 
 
-    }
+
+
+
+
 }
-
-
-// Facebook Login
-//        callbackManager = CallbackManager.Factory.create();
-//
-//        imgFB.setOnClickListener(new View.OnClickListener() {
-//        binding.btnLogin.setOnClickListener(new View.OnClickListener()
-//            {
-//                @Override
-//                public void onClick (View view){
-//                if (validateInput()) {
-//                    // Nếu dữ liệu hợp lệ, hiển thị thông báo thành công + xử lí tiếp...
-//                    Toast.makeText(getActivity(), "Login successful", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    // Nếu dữ liệu không hợp lệ, hiển thị thông báo lỗi
-//                    Toast.makeText(getActivity(), "Invalid input, login failed", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//                private boolean validateInput () {
-//                String usernameOrEmail = binding.edtUsername.getText().toString().trim();
-//                String password = binding.edtPassword.getText().toString().trim();
-//
-//                // Kiểm tra Username hoặc Email
-//                if (usernameOrEmail.isEmpty()) {
-//                    binding.edtUsername.setError("Username or Email cannot be empty");
-//                    return false;
-//                }
-//
-//                // Kiểm tra định dạng email nếu input có chứa ký tự '@'
-//                if (usernameOrEmail.contains("@") && !Patterns.EMAIL_ADDRESS.matcher(usernameOrEmail).matches()) {
-//                    binding.edtUsername.setError("Invalid email format");
-//                    return false;
-//                }
-//
-//                // Kiểm tra Password
-//                if (password.isEmpty()) {
-//                    binding.edtPassword.setError("Password cannot be empty");
-//                    return false;
-//                }
-//
-//                if (password.length() < 6) {
-//                    binding.edtPassword.setError("Password must be at least 6 characters long");
-//                    return false;
-//                }
-//
-//                // Nếu tất cả đều hợp lệ
-//                binding.edtUsername.setError(null);
-//                binding.edtPassword.setError(null);
-//                return true;
-//            }
-//            });
-//        binding.tvForgotPassword.setOnClickListener(new View.OnClickListener()
-//
-//            {
-//
-//                @Override
-//                public void onClick (View v){
-//                LoginManager.getInstance().logInWithReadPermissions(
-//                        getActivity(), Arrays.asList("email", "public_profile")
-//                );
-//            }
-//            });
-
-//        LoginManager.getInstance().
-//
-//            registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-//                @Override
-//                public void onSuccess (LoginResult loginResult){
-//                    handleFacebookAccessToken(loginResult.getAccessToken());
-//                }
-//
-//                @Override
-//                public void onCancel () {
-//                    Toast.makeText(getContext(), "Đăng nhập bị hủy.", Toast.LENGTH_SHORT).show();
-//                }
-//
-//                @Override
-//                public void onError (FacebookException error){
-//                    Toast.makeText(getContext(), "Đăng nhập Facebook không thành công.", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-
-
-
-//
-
