afcfb0ee9000b6a5be611340d63d63dc143841bc
duykhanh <duykhanhz112233@gmail.com>
2024-08-30 09:07:56 +0700

    Register user and save to DB

--- app/src/main/java/com/example/gymfitness/retrofit/GymApi.java
+++ app/src/main/java/com/example/gymfitness/retrofit/GymApi.java
@@ -1,10 +1,23 @@
 package com.example.gymfitness.retrofit;
 
+import com.example.gymfitness.data.ApiResponse;
+
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface GymApi {
 
     @GET("your_endpoint")
     Call<String> getData() ;
+
+    @FormUrlEncoded
+    @POST("/api/signup")
+    Call<ApiResponse> signup(
+            @Field("user_email") String userEmail,
+            @Field("user_fullname") String userFullName,
+            @Field("user_id") String userId
+    );
 }
--- app/src/main/java/com/example/gymfitness/viewmodels/SignUpViewModel.java
+++ app/src/main/java/com/example/gymfitness/viewmodels/SignUpViewModel.java
@@ -6,12 +6,16 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.gymfitness.data.ApiResponse;
 import com.example.gymfitness.data.SignUpCallback;
 import com.example.gymfitness.data.UserSignUp;
 import com.example.gymfitness.data.Users;
+import com.example.gymfitness.retrofit.GymApi;
+import com.example.gymfitness.retrofit.RetrofitInstance;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -19,8 +23,10 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
@@ -28,18 +34,23 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class SignUpViewModel extends ViewModel {
 
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
-    private MutableLiveData<UserSignUp> user = new MutableLiveData<>();
+    private MutableLiveData<UserSignUp> user;
     private GoogleSignInClient googleSignInClient;
     private CallbackManager callbackManager;
 
     public static final int RC_SIGN_IN = 40;
-
+    private final GymApi gymApi= RetrofitInstance.getApiService();
     public SignUpViewModel() {
         mAuth = FirebaseAuth.getInstance();
+        user=  new MutableLiveData<>();
         database = FirebaseDatabase.getInstance();
     }
 
@@ -138,8 +149,54 @@ public class SignUpViewModel extends ViewModel {
         this.user.setValue(userSignUp);
     }
 
+    public void saveDB(SignUpCallback signUpCallback){
+        Call<ApiResponse> call=gymApi.signup(user.getValue().getEmail(),user.getValue().getFullName(), mAuth.getUid());
+        call.enqueue(new Callback<ApiResponse>() {
+            @Override
+            public void onResponse(@NonNull Call<ApiResponse> call, @NonNull Response<ApiResponse> response) {
+                signUpCallback.onSuccess();
+                mAuth.signOut();
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<ApiResponse> call, @NonNull Throwable t) {
+                signUpCallback.onFailure(-1000);
+            }
+        });
+    }
+
     public boolean checkUserStatus() {
         return mAuth.getCurrentUser() != null;
+    }
+
+    public void deleteUserFromFirebase(SignUpCallback signUpCallback){
+        FirebaseUser firebaseUser=mAuth.getCurrentUser();
+        if(firebaseUser!=null){
+            AuthCredential credential= EmailAuthProvider
+                    .getCredential(user.getValue().getEmail(),user.getValue().getPassword());
+
+            firebaseUser.reauthenticate(credential)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            firebaseUser.delete()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                signUpCallback.onSuccess();
+                                                mAuth.signOut();
+                                            }
+                                            else{
+                                                signUpCallback.onFailure(-1000);
+                                                mAuth.signOut();
+                                            }
+                                        }
+                                    });
+                        }
+                    });
+        }
+
     }
 }
 
