e8398d12109a86a09e50f6afc897ce1706acf2d0
duykhanh <duykhanhz112233@gmail.com>
2024-08-30 14:41:44 +0700

    add apiResponse

---
+++ app/src/main/java/com/example/gymfitness/data/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.example.gymfitness.data;
+
+public class ApiResponse {
+    private String message;
+
+    // Getter và setter
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
