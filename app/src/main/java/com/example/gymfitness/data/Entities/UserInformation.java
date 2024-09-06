5088c4e2b9eaa889efd0bff0a2d6d252934e9d5a
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-09-06 15:47:06 +0700

    Create UserInformation.java

---
+++ app/src/main/java/com/example/gymfitness/data/Entities/UserInformation.java
@@ -0,0 +1,93 @@
+package com.example.gymfitness.data.Entities;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class UserInformation implements Serializable {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String gender;
+    private String age;
+    private int weight;
+    private int height;
+    private String goal;
+    private Date DoB;
+    private String img;
+    private String level;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+
+    public Date getDoB() {
+        return DoB;
+    }
+
+    public void setDoB(Date doB) {
+        DoB = doB;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
