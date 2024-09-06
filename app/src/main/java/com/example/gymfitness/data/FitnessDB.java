960e1b916ea5d267e4cd1388bf5fc23e6f17a61d
bqdiu <boquangdieu2003@gmail.com>
2024-09-06 15:48:29 +0700

    add fitnessDB class

--- app/src/main/java/com/example/gymfitness/data/FitnessDB.java
+++ app/src/main/java/com/example/gymfitness/data/FitnessDB.java
@@ -1,4 +1,33 @@
 package com.example.gymfitness.data;
 
-public class FitnessDB{
+import android.content.Context;
+
+import androidx.room.AutoMigration;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+//@Database(
+//        entities = {UserInformation.class},
+//        version = 1,
+//        autoMigrations = {
+//                @AutoMigration(from = 1, to = 2)
+//        }
+//)
+public abstract class FitnessDB extends RoomDatabase {
+    private static final String DATABASE_NAME = "gym_fitness.db";
+    private static FitnessDB instance;
+
+    public static synchronized FitnessDB getInstance(Context context)
+    {
+        if(instance == null)
+        {
+             instance = Room.databaseBuilder(context.getApplicationContext(),FitnessDB.class,DATABASE_NAME)
+                     .allowMainThreadQueries()
+                     .build();
+        }
+        return instance;
+    }
+
+
 }
