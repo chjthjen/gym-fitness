d9d05bdaf3c4977e6a65d7be94a2e1d97a9378e9
bqdiu <boquangdieu2003@gmail.com>
2024-09-12 23:10:53 +0700

    fix binding define

--- app/src/main/java/com/example/gymfitness/fragments/floatingMenu/EditProfileFragment.java
+++ app/src/main/java/com/example/gymfitness/fragments/floatingMenu/EditProfileFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,18 +13,18 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gymfitness.R;
+import com.example.gymfitness.databinding.FragmentEditprofileBinding;
 
 import java.util.Objects;
 
 public class EditProfileFragment extends Fragment {
 
-
+    FragmentEditprofileBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_6_1_1_a, container, false);
+        return inflater.inflate(R.layout.fragment_editprofile, container, false);
     }
 
     @Override
