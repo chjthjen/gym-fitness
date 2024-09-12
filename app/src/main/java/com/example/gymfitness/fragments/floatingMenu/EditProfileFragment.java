f14e2d05bb569e4f526b6818960e1209b5c72046
npbn42 <148618708+npbn42@users.noreply.github.com>
2024-09-12 23:08:04 +0700

    update layout edit profile

--- app/src/main/java/com/example/gymfitness/fragments/floatingMenu/EditProfileFragment.java
+++ app/src/main/java/com/example/gymfitness/fragments/floatingMenu/EditProfileFragment.java
@@ -2,6 +2,9 @@ package com.example.gymfitness.fragments.floatingMenu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -10,7 +13,10 @@ import android.view.ViewGroup;
 
 import com.example.gymfitness.R;
 
+import java.util.Objects;
+
 public class EditProfileFragment extends Fragment {
+
 
 
     @Override
@@ -18,5 +24,18 @@ public class EditProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_6_1_1_a, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).hide();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).show();
+
     }
 }
