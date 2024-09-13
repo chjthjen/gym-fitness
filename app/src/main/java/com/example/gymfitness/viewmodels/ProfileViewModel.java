6eccc424248e3c8d84f5d1a949128db9d7c20f1b
npbn42 <148618708+npbn42@users.noreply.github.com>
2024-09-13 17:11:50 +0700

    load edit profile

--- app/src/main/java/com/example/gymfitness/viewmodels/ProfileViewModel.java
+++ app/src/main/java/com/example/gymfitness/viewmodels/ProfileViewModel.java
@@ -1,8 +1,28 @@
 package com.example.gymfitness.viewmodels;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.gymfitness.data.DAO.UserInformationDAO;
+import com.example.gymfitness.data.entities.UserInformation;
 
 public class ProfileViewModel extends ViewModel {
 
+    private UserInformationDAO userInformationDAO;
+    private MutableLiveData<UserInformation> userInformation;
 
+    public ProfileViewModel(UserInformationDAO userInformationDAO){
+        this.userInformationDAO = userInformationDAO;
+        userInformation = new MutableLiveData<>();
+    }
+
+    private void loadUserInfo(){
+        UserInformation information = userInformationDAO.getUserInformation();
+        userInformation.setValue(information);
+    }
+
+    public LiveData<UserInformation> getUserInformation() {
+        return userInformation;
+    }
 }
