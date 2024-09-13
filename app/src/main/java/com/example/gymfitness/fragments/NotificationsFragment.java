d7c8ca8d2a6d5561a17cd29181b55a58eeeec674
vtttruc <vothithanhtruc0307@gmail.com>
2024-09-13 21:51:33 +0700

    noi fragment setting voi notification

--- app/src/main/java/com/example/gymfitness/fragments/NotificationsFragment.java
+++ app/src/main/java/com/example/gymfitness/fragments/NotificationsFragment.java
@@ -2,9 +2,13 @@ package com.example.gymfitness.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -53,49 +57,84 @@ public class NotificationsFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
 
+        viewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
         binding.imgGeneralNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.toggleLockScreen(binding.imgGeneralNotification, isGeneralNotificationOn);
+                if (viewModel != null) {
+                    viewModel.toggleLockScreen(binding.imgGeneralNotification, isGeneralNotificationOn);
+                    isGeneralNotificationOn = !isGeneralNotificationOn;
+                } else {
+                    Log.e("NotificationsFragment", "ViewModel is null!");
+                }
             }
-            });
-
+        });
         binding.imgSound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.toggleLockScreen(binding.imgSound, isSoundOn);
+                if (viewModel != null) {
+                    viewModel.toggleLockScreen(binding.imgSound, isSoundOn);
+                    isSoundOn = !isSoundOn;
+                } else {
+                    Log.e("NotificationsFragment", "ViewModel is null!");
+                }
             }
         });
 
         binding.imfDontDisturbMode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.toggleLockScreen(binding.imfDontDisturbMode, isDontDisturbOn);
+                if (viewModel != null) {
+                    viewModel.toggleLockScreen(binding.imfDontDisturbMode, isDontDisturbOn);
+                    isDontDisturbOn = !isDontDisturbOn;
+                } else {
+                    Log.e("NotificationsFragment", "ViewModel is null!");
+                }
             }
         });
 
         binding.imgVibrate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.toggleLockScreen(binding.imgVibrate, isVibrateOn);
+                if (viewModel != null) {
+                    viewModel.toggleLockScreen(binding.imgVibrate, isVibrateOn);
+                    isVibrateOn = !isVibrateOn;
+                } else {
+                    Log.e("NotificationsFragment", "ViewModel is null!");
+                }
             }
         });
 
         binding.imgLockScreen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.toggleLockScreen(binding.imgLockScreen, isLockScreenOn);
+                if (viewModel != null) {
+                    viewModel.toggleLockScreen(binding.imgLockScreen, isLockScreenOn);
+                    isLockScreenOn = !isLockScreenOn;
+                }else {
+                    Log.e("NotificationsFragment", "ViewModel is null!");
+                }
             }
         });
 
         binding.imgReminders.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                viewModel.toggleLockScreen(binding.imgReminders, isRemindersOn);
+                if (viewModel != null) {
+                    viewModel.toggleLockScreen(binding.imgReminders, isRemindersOn);
+                    isRemindersOn = !isRemindersOn;
+                }else {
+                    Log.e("NotificationsFragment", "ViewModel is null!");
                 }
+            }
         });
-
-        return binding.getRoot();
     }
-
 }
--- app/src/main/java/com/example/gymfitness/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/gymfitness/fragments/SettingsFragment.java
@@ -2,13 +2,18 @@ package com.example.gymfitness.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.gymfitness.R;
+import com.example.gymfitness.databinding.FragmentSettingsBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +30,9 @@ public class SettingsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private NavController navController;
+    private FragmentSettingsBinding binding;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -61,6 +69,16 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        binding = FragmentSettingsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(view);
+        if (binding != null && binding.btnNotification != null) {
+            binding.btnNotification.setOnClickListener(v -> navController.navigate(R.id.action_settingsFragment_to_notificationFrag));
+        }
     }
 }
