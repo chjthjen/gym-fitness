03221f0edae8d54146b8a9ac9f719db5411ef1c3
vuhuyenvi <153046776+vuhuyenvi@users.noreply.github.com>
2024-09-13 17:49:38 +0700

    load profile

--- app/src/main/java/com/example/gymfitness/viewmodels/ProfileViewModel.java
+++ app/src/main/java/com/example/gymfitness/viewmodels/ProfileViewModel.java
@@ -1,8 +1,20 @@
 package com.example.gymfitness.viewmodels;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.gymfitness.data.DAO.UserInformationDAO;
+import com.example.gymfitness.data.entities.UserInformation;
+
 public class ProfileViewModel extends ViewModel {
+    private final MutableLiveData<UserInformation> userInformationLiveData = new MutableLiveData<>();
 
+    public LiveData<UserInformation> getUserInformation() {
+        return userInformationLiveData;
+    }
 
+    public void setUserInformation(UserInformation userInformation) {
+        userInformationLiveData.setValue(userInformation);
+    }
 }
