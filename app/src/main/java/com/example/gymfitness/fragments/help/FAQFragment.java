32bead4b7fc5cdb7fd95ddffc261c4083742209d
DuyCorn <ngohoangduy0103@gmail.com>
2024-09-15 01:48:15 +0700

    Fix FAQ

--- app/src/main/java/com/example/gymfitness/fragments/help/FAQFragment.java
+++ app/src/main/java/com/example/gymfitness/fragments/help/FAQFragment.java
@@ -1,57 +1,133 @@
 package com.example.gymfitness.fragments.help;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 import com.example.gymfitness.R;
-import com.example.gymfitness.adapters.FAQAdapter;
-import com.example.gymfitness.databinding.FragmentFaqBinding;
-
-import java.util.Arrays;
-import java.util.List;
+import com.example.gymfitness.adapters.help.FAQViewpagerAdapter;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class FAQFragment extends Fragment {
-    FragmentFaqBinding binding;
 
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private FAQViewpagerAdapter adapter;
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_faq, container, false);
-        binding = FragmentFaqBinding.bind(view);
-        return binding.getRoot();
+
+        tabLayout = view.findViewById(R.id.tabLayout);
+        viewPager2 = view.findViewById(R.id.viewPager);
+        adapter = new FAQViewpagerAdapter(this);
+
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, (tab, position) -> {
+            // Tạo tab tùy chỉnh
+            View customTab = LayoutInflater.from(getContext()).inflate(R.layout.faq_tab, null);
+            TextView textView = customTab.findViewById(R.id.tabTextView);
+
+            switch (position) {
+                case 0:
+                    textView.setText("General");
+                    break;
+                case 1:
+                    textView.setText("Account");
+                    break;
+                case 2:
+                    textView.setText("Services");
+                    break;
+            }
+
+            tab.setCustomView(customTab);
+        }).attach();
+
+        setInitialTabStyle();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                updateTabStyles();
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                updateTabStyles();
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+        setTabSpacing(tabLayout);
+        return view;
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        FAQAdapter adapter;
-
-        List<String> titles = Arrays.asList(
-                getString(R.string.lorem_ipsum_dolor_sit_amet),
-                getString(R.string.lorem_ipsum_dolor_sit_amet),
-                getString(R.string.lorem_ipsum_dolor_sit_amet),
-                getString(R.string.lorem_ipsum_dolor_sit_amet),
-                getString(R.string.lorem_ipsum_dolor_sit_amet),
-                getString(R.string.lorem_ipsum_dolor_sit_amet));
-        List<String> contents = Arrays.asList(
-                getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_n_sed_do_eiusmod_tempor_incididunt_ut_labore_et_dolore_n_magna_aliqua),
-                getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_n_sed_do_eiusmod_tempor_incididunt_ut_labore_et_dolore_n_magna_aliqua),
-                getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_n_sed_do_eiusmod_tempor_incididunt_ut_labore_et_dolore_n_magna_aliqua),
-                getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_n_sed_do_eiusmod_tempor_incididunt_ut_labore_et_dolore_n_magna_aliqua),
-                getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_n_sed_do_eiusmod_tempor_incididunt_ut_labore_et_dolore_n_magna_aliqua),
-                getString(R.string.lorem_ipsum_dolor_sit_amet_consectetur_adipiscing_elit_n_sed_do_eiusmod_tempor_incididunt_ut_labore_et_dolore_n_magna_aliqua)
-        );
-
-        adapter = new FAQAdapter(getActivity(), titles, contents);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-        binding.reVFAQ.setLayoutManager(linearLayoutManager);
-        binding.reVFAQ.setAdapter(adapter);
+    private void setInitialTabStyle() {
+        TabLayout.Tab firstTab = tabLayout.getTabAt(0);
+        if (firstTab != null) {
+            View customView = firstTab.getCustomView();
+            if (customView != null) {
+                TextView textView = customView.findViewById(R.id.tabTextView);
+                textView.setBackgroundResource(R.drawable.faq_type_selected);
+                textView.setTextColor(getResources().getColor(R.color.black));
+            }
+        }
     }
+
+    private void updateTabStyles() {
+        for (int i = 0; i < tabLayout.getTabCount(); i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                View customView = tab.getCustomView();
+                if (customView != null) {
+                    TextView textView = customView.findViewById(R.id.tabTextView);
+                    if (tab.isSelected()) {
+                        textView.setBackgroundResource(R.drawable.faq_type_selected);
+                        textView.setTextColor(getResources().getColor(R.color.black));
+                    } else {
+                        textView.setBackgroundResource(R.drawable.faq_type_no_selected);
+                        textView.setTextColor(getResources().getColor(R.color.lightpurple));
+                    }
+                }
+            }
+        }
+    }
+
+    private void setTabSpacing(TabLayout tabLayout) {
+        final int tabCount = tabLayout.getTabCount();
+        for (int i = 0; i < tabCount; i++) {
+            TabLayout.Tab tab = tabLayout.getTabAt(i);
+            if (tab != null) {
+                View tabView = ((ViewGroup) tabLayout.getChildAt(0)).getChildAt(i);
+                if (tabView != null) {
+                    ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) tabView.getLayoutParams();
+                    if (i == 1) {
+                        params.setMargins(dpToPx(7), 0, dpToPx(7), 0);
+                    } else {
+                        params.setMargins(0, 0, 0, 0);
+                    }
+                    tabView.setLayoutParams(params);
+                }
+            }
+        }
+    }
+    public int dpToPx(int dp) {
+        Resources resources = getResources();
+        return Math.round(dp * (resources.getDisplayMetrics().xdpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+
 }
