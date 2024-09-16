2ec26bd468152fbaf9f211c3a727932603fe7213
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2024-09-16 15:29:52 +0700

    add FavoriteHelper

--- app/src/main/java/com/example/gymfitness/data/DAO/FavoriteArticleDAO.java
+++ app/src/main/java/com/example/gymfitness/data/DAO/FavoriteArticleDAO.java
@@ -1,6 +1,7 @@
 package com.example.gymfitness.data.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
 
 import com.example.gymfitness.data.entities.FavoriteArticle;
@@ -11,4 +12,10 @@ import java.util.List;
 public interface FavoriteArticleDAO {
     @Query("SELECT * FROM FavoriteArticle")
     List<FavoriteArticle> getAll();
+
+    @Query("DELETE FROM FavoriteArticle WHERE article_name = :article_name")
+    void delete(String article_name);
+
+    @Insert
+    void insert(FavoriteArticle favoriteArticle);
 }
--- app/src/main/java/com/example/gymfitness/data/DAO/FavoriteWorkoutDAO.java
+++ app/src/main/java/com/example/gymfitness/data/DAO/FavoriteWorkoutDAO.java
@@ -1,6 +1,7 @@
 package com.example.gymfitness.data.DAO;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
 import androidx.room.Query;
 
 import com.example.gymfitness.data.entities.FavoriteWorkout;
@@ -11,4 +12,10 @@ import java.util.List;
 public interface FavoriteWorkoutDAO {
     @Query("SELECT * FROM FavoriteWorkout")
     List<FavoriteWorkout> getAll();
+
+    @Insert
+    void insert(FavoriteWorkout favoriteWorkout);
+
+    @Query("DELETE FROM FavoriteWorkout WHERE workout_name = :workout_name")
+    void delete(String workout_name);
 }
