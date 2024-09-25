1bc3808446b591afe4e931a27cc0fb71e1e9ff68
bqdiu <boquangdieu2003@gmail.com>
2024-09-25 12:19:23 +0700

    update notification control

--- app/src/main/java/com/example/gymfitness/helpers/NotificationHelper.java
+++ app/src/main/java/com/example/gymfitness/helpers/NotificationHelper.java
@@ -1,10 +1,17 @@
 package com.example.gymfitness.helpers;
 
 
+import android.Manifest;
+import android.app.ActivityManager;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.os.Build;
 import android.provider.Settings;
+
+import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationHelper {
 
@@ -49,6 +56,47 @@ public class NotificationHelper {
                 notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
             }
         }
+    }
+
+    public static boolean isAppVolumeOn(Context context) {
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        // Check the music stream volume
+        int volume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        return volume != 0;
+    }
+    public static void toggleAppVolume(Context context) {
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        if (currentVolume == 0) {
+            // If the volume is off, turn it on to the maximum volume
+            int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, maxVolume, 0);
+        } else {
+            // If the volume is on, turn it off
+            audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
+        }
+    }
+
+
+    public static boolean isPostNotificationsPermissionGranted(Context context) {
+        PackageManager pm = context.getPackageManager();
+        int hasPerm = pm.checkPermission(
+                "android.permission.POST_NOTIFICATIONS",
+                context.getPackageName());
+        return hasPerm == PackageManager.PERMISSION_GRANTED;
+    }
+    public static boolean areNotificationsEnabled(Context context) {
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        return notificationManager.areNotificationsEnabled();
+    }
+    public static void toggleNotificationPermission(Context context) {
+        Intent intent = new Intent();
+        intent.setAction("android.settings.APP_NOTIFICATION_SETTINGS");
+
+        // for Android 8 and above
+        intent.putExtra("android.provider.extra.APP_PACKAGE", context.getPackageName());
+
+        context.startActivity(intent);
     }
 
 }
